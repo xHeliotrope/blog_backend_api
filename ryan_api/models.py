ac3d45df40f1084110d1a9b9d8b21ae626a7c5cd
Ryan Moore <rmoore@dom.wustl.edu>
2017-07-18 01:20:46 -0500

    added endpoint for runs and reads

--- ryan_api/models.py
+++ ryan_api/models.py
@@ -6,7 +6,7 @@ class Run(models.Model):
     # distance ran in miles
     distance = models.FloatField(null=False)
     # date of the workout
-    date = models.DateField(auto_now=False, auto_add_now=False, null=False)
+    date = models.DateField(null=False)
 
     class Meta:
         db_table = 'runs'
@@ -14,7 +14,7 @@ class Run(models.Model):
 
 
 class Read(models.Model):
-    book = models.TextField(blank=False, null=False)
+    title = models.TextField(blank=False, null=False)
     author = models.TextField(blank=False, null=False)
     is_finished = models.BooleanField()
 
--- ryan_api/views.py
+++ ryan_api/views.py
@@ -1,3 +1,20 @@
+from django.views.decorators.csrf import csrf_protect
+from django.db import connection 
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+@csrf_protect
+def give_ryan(request):
+    if request.method == "GET":
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT row_to_json(rows) FROM (SELECT * from runs) as rows")
+            runs = [result[0] for result in cursor.fetchall()]
+            runs = [{k:v for k,v in run.items() if k!='id'} for run in runs]
+            cursor.execute("SELECT row_to_json(rows) FROM (SELECT * from reads) as rows")
+            books = [result[0] for result in cursor.fetchall()]
+            books = [{k:v for k,v in book.items() if k!='id'} for book in books]
+            
+        return HttpResponse(json.dumps({"books": books, "runs": runs}), content_type="application/json")
+    else:
+        return HttpResponse("gets only please", content_type="text/plain")
